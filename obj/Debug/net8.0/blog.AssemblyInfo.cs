e1fb17475d0e5c8cfb9f5be131bd15584087b14b
Acestus <architect@acestus.com>
2024-12-28 00:27:25 -0600

    Daily Commit

--- obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61260728acf7a70c209dc55961015d1c44942269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c593b6ba246f4ab461efa7acaee1b9415b6ec29")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
